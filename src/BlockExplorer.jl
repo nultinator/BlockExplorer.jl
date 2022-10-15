8bfd3da586f7c486974d75cc3a8a560095d75262
Jake Nulty <nultinator@yahoo.com>
2022-10-14 22:37:16 -0400

    add: Komodo (coin)

--- src/BlockExplorer.jl
+++ src/BlockExplorer.jl
@@ -5,7 +5,7 @@ using JSON
 include("API.jl")
 
 # coins
-export Coin, BTC, BCH, LTC, YEC, ZEC
+export Coin, BTC, BCH, KMD, LTC, YEC, ZEC
 # api
 export getblockcount, getblock_hash, getblock, getaddress, listunspent, getbalance_history
 export gettransaction
